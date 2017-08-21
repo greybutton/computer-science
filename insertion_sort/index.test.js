ef6b0d5c14c396e6a6534196384b8a4b30fa6e3a
gb <gb>
2017-08-21 12:22:45 +0800

    Refactoring tests
    Change testList and resultList from array to object
    Add real numbers test
    Add repeat real number test
    Add negative numbers test
    Add repeat negative number test
    Add words test
    Add repeat word test

--- insertion_sort/index.test.js
+++ insertion_sort/index.test.js
@@ -1,36 +1,85 @@
 import insertionSort from './index';
 
-describe('test insertion sort', () => {
-  const testList = [
-    [9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
-    [1, 8, 4, 2, 3, 7, 5, 6, 9, 0],
-    [5, 2, 4, 6, 1, 3],
-    [10, 8, 2, 12, 5, 100],
-  ];
-  const resultList = [
-    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [1, 2, 3, 4, 5, 6],
-    [2, 5, 8, 10, 12, 100],
-  ];
-  test(`[${testList[0]}] should be [${resultList[0]}]`, () => {
-    const received = insertionSort(testList[0]);
-    const expected = resultList[0];
+describe('insertion sort', () => {
+  const testList = {
+    one: [123],
+    simple: [5, 2, 4, 6, 1, 3],
+    reverse: [9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
+    sorted: [1, 8, 4, 2, 3, 7, 5, 6, 9, 0],
+    repeat: [10, 8, 2, 12, 5, 10],
+    real: [0.63, 0.19, 0.46, 0.61, 0.85, 0.4, 0.6, 0.71, 0.41],
+    realRepeat: [0.41, 0.19, 0.46, 0.61, 0.85, 0.4, 0.6, 0.71, 0.41],
+    negative: [-5, 2, 4, 6, 1, 3],
+    negativeRepeat: [-5, 2, 4, 1, 3, -5],
+    words: ['aba', 'aaa', 'bbb'],
+    wordsRepeat: ['aba', 'aaa', 'bbb', 'aba'],
+  };
+  const resultList = {
+    one: [123],
+    simple: [1, 2, 3, 4, 5, 6],
+    reverse: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    sorted: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    repeat: [2, 5, 8, 10, 10, 12],
+    real: [0.19, 0.4, 0.41, 0.46, 0.6, 0.61, 0.63, 0.71, 0.85],
+    realRepeat: [0.19, 0.4, 0.41, 0.41, 0.46, 0.6, 0.61, 0.71, 0.85],
+    negative: [-5, 1, 2, 3, 4, 6],
+    negativeRepeat: [-5, -5, 1, 2, 3, 4],
+    words: ['aaa', 'aba', 'bbb'],
+    wordsRepeat: ['aaa', 'aba', 'aba', 'bbb'],
+  };
+  test(`One number [${testList.one}] should be [${resultList.one}]`, () => {
+    const received = insertionSort(testList.one);
+    const expected = resultList.one;
     expect(received).toEqual(expected);
   });
-  test(`[${testList[1]}] should be [${resultList[1]}]`, () => {
-    const received = insertionSort(testList[1]);
-    const expected = resultList[1];
+  test(`Simple array [${testList.simple}] should be [${resultList.simple}]`, () => {
+    const received = insertionSort(testList.simple);
+    const expected = resultList.simple;
     expect(received).toEqual(expected);
   });
-  test(`[${testList[2]}] should be [${resultList[2]}]`, () => {
-    const received = insertionSort(testList[2]);
-    const expected = resultList[2];
+  test(`Reverse array [${testList.reverse}] should be [${resultList.reverse}]`, () => {
+    const received = insertionSort(testList.reverse);
+    const expected = resultList.reverse;
     expect(received).toEqual(expected);
   });
-  test(`[${testList[3]}] should be [${resultList[3]}]`, () => {
-    const received = insertionSort(testList[3]);
-    const expected = resultList[3];
+  test(`Sorted array [${testList.sorted}] should be [${resultList.sorted}]`, () => {
+    const received = insertionSort(testList.sorted);
+    const expected = resultList.sorted;
+    expect(received).toEqual(expected);
+  });
+  test(`Repeat number ${testList.repeat[0]} [${testList.repeat}] should be [${resultList.repeat}]`, () => {
+    const received = insertionSort(testList.repeat);
+    const expected = resultList.repeat;
+    expect(received).toEqual(expected);
+  });
+  test(`Real numbers [${testList.real}] should be [${resultList.real}]`, () => {
+    const received = insertionSort(testList.real);
+    const expected = resultList.real;
+    expect(received).toEqual(expected);
+  });
+  test(`Repeat real number ${testList.realRepeat[0]} [${testList.realRepeat}] should be [${resultList.realRepeat}]`, () => {
+    const received = insertionSort(testList.realRepeat);
+    const expected = resultList.realRepeat;
+    expect(received).toEqual(expected);
+  });
+  test(`Negative number ${testList.negative[0]}  [${testList.negative}] should be [${resultList.negative}]`, () => {
+    const received = insertionSort(testList.negative);
+    const expected = resultList.negative;
+    expect(received).toEqual(expected);
+  });
+  test(`Repeat negative number ${testList.negativeRepeat[0]} [${testList.negativeRepeat}] should be [${resultList.negativeRepeat}]`, () => {
+    const received = insertionSort(testList.negativeRepeat);
+    const expected = resultList.negativeRepeat;
+    expect(received).toEqual(expected);
+  });
+  test(`Words array [${testList.words}] should be [${resultList.words}]`, () => {
+    const received = insertionSort(testList.words);
+    const expected = resultList.words;
+    expect(received).toEqual(expected);
+  });
+  test(`Repeat word ${testList.wordsRepeat[0]} [${testList.wordsRepeat}] should be [${resultList.wordsRepeat}]`, () => {
+    const received = insertionSort(testList.wordsRepeat);
+    const expected = resultList.wordsRepeat;
     expect(received).toEqual(expected);
   });
 });
--- selection_sort/index.test.js
+++ selection_sort/index.test.js
@@ -1,43 +1,85 @@
 import selectionSort from './index';
 
 describe('selection sort', () => {
-  const testList = [
-    [9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
-    [1, 8, 4, 2, 3, 7, 5, 6, 9, 0],
-    [5, 2, 4, 6, 1, 3],
-    [10, 8, 2, 12, 5, 10],
-    [123],
-  ];
-  const resultList = [
-    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [1, 2, 3, 4, 5, 6],
-    [2, 5, 8, 10, 10, 12],
-    [123],
-  ];
-  test(`[${testList[0]}] should be [${resultList[0]}]`, () => {
-    const received = selectionSort(testList[0], 0, testList[0].length);
-    const expected = resultList[0];
+  const testList = {
+    one: [123],
+    simple: [5, 2, 4, 6, 1, 3],
+    reverse: [9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
+    sorted: [1, 8, 4, 2, 3, 7, 5, 6, 9, 0],
+    repeat: [10, 8, 2, 12, 5, 10],
+    real: [0.63, 0.19, 0.46, 0.61, 0.85, 0.4, 0.6, 0.71, 0.41],
+    realRepeat: [0.41, 0.19, 0.46, 0.61, 0.85, 0.4, 0.6, 0.71, 0.41],
+    negative: [-5, 2, 4, 6, 1, 3],
+    negativeRepeat: [-5, 2, 4, 1, 3, -5],
+    words: ['aba', 'aaa', 'bbb'],
+    wordsRepeat: ['aba', 'aaa', 'bbb', 'aba'],
+  };
+  const resultList = {
+    one: [123],
+    simple: [1, 2, 3, 4, 5, 6],
+    reverse: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    sorted: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    repeat: [2, 5, 8, 10, 10, 12],
+    real: [0.19, 0.4, 0.41, 0.46, 0.6, 0.61, 0.63, 0.71, 0.85],
+    realRepeat: [0.19, 0.4, 0.41, 0.41, 0.46, 0.6, 0.61, 0.71, 0.85],
+    negative: [-5, 1, 2, 3, 4, 6],
+    negativeRepeat: [-5, -5, 1, 2, 3, 4],
+    words: ['aaa', 'aba', 'bbb'],
+    wordsRepeat: ['aaa', 'aba', 'aba', 'bbb'],
+  };
+  test(`One number [${testList.one}] should be [${resultList.one}]`, () => {
+    const received = selectionSort(testList.one, 0, testList.one.length);
+    const expected = resultList.one;
     expect(received).toEqual(expected);
   });
-  test(`[${testList[1]}] should be [${resultList[1]}]`, () => {
-    const received = selectionSort(testList[1], 0, testList[1].length);
-    const expected = resultList[1];
+  test(`Simple array [${testList.simple}] should be [${resultList.simple}]`, () => {
+    const received = selectionSort(testList.simple, 0, testList.simple.length);
+    const expected = resultList.simple;
     expect(received).toEqual(expected);
   });
-  test(`[${testList[2]}] should be [${resultList[2]}]`, () => {
-    const received = selectionSort(testList[2], 0, testList[2].length);
-    const expected = resultList[2];
+  test(`Reverse array [${testList.reverse}] should be [${resultList.reverse}]`, () => {
+    const received = selectionSort(testList.reverse, 0, testList.reverse.length);
+    const expected = resultList.reverse;
     expect(received).toEqual(expected);
   });
-  test(`[${testList[3]}] should be [${resultList[3]}]`, () => {
-    const received = selectionSort(testList[3], 0, testList[3].length);
-    const expected = resultList[3];
+  test(`Sorted array [${testList.sorted}] should be [${resultList.sorted}]`, () => {
+    const received = selectionSort(testList.sorted, 0, testList.sorted.length);
+    const expected = resultList.sorted;
     expect(received).toEqual(expected);
   });
-  test(`[${testList[4]}] should be [${resultList[4]}]`, () => {
-    const received = selectionSort(testList[4], 0, testList[4].length);
-    const expected = resultList[4];
+  test(`Repeat number ${testList.repeat[0]} [${testList.repeat}] should be [${resultList.repeat}]`, () => {
+    const received = selectionSort(testList.repeat, 0, testList.repeat.length);
+    const expected = resultList.repeat;
+    expect(received).toEqual(expected);
+  });
+  test(`Real numbers [${testList.real}] should be [${resultList.real}]`, () => {
+    const received = selectionSort(testList.real, 0, testList.real.length);
+    const expected = resultList.real;
+    expect(received).toEqual(expected);
+  });
+  test(`Repeat real number ${testList.realRepeat[0]} [${testList.realRepeat}] should be [${resultList.realRepeat}]`, () => {
+    const received = selectionSort(testList.realRepeat, 0, testList.realRepeat.length);
+    const expected = resultList.realRepeat;
+    expect(received).toEqual(expected);
+  });
+  test(`Negative number ${testList.negative[0]}  [${testList.negative}] should be [${resultList.negative}]`, () => {
+    const received = selectionSort(testList.negative, 0, testList.negative.length);
+    const expected = resultList.negative;
+    expect(received).toEqual(expected);
+  });
+  test(`Repeat negative number ${testList.negativeRepeat[0]} [${testList.negativeRepeat}] should be [${resultList.negativeRepeat}]`, () => {
+    const received = selectionSort(testList.negativeRepeat, 0, testList.negativeRepeat.length);
+    const expected = resultList.negativeRepeat;
+    expect(received).toEqual(expected);
+  });
+  test(`Words array [${testList.words}] should be [${resultList.words}]`, () => {
+    const received = selectionSort(testList.words, 0, testList.words.length);
+    const expected = resultList.words;
+    expect(received).toEqual(expected);
+  });
+  test(`Repeat word ${testList.wordsRepeat[0]} [${testList.wordsRepeat}] should be [${resultList.wordsRepeat}]`, () => {
+    const received = selectionSort(testList.wordsRepeat, 0, testList.wordsRepeat.length);
+    const expected = resultList.wordsRepeat;
     expect(received).toEqual(expected);
   });
 });
